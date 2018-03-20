23d9f1cd7a01005e4da63270f0445c379f9e0321
enricovianello <maverick17586@gmail.com>
2018-03-20 10:14:20 +0100

    Fixed push-image

--- docker/storm-testsuite/push-image.sh
+++ docker/storm-testsuite/push-image.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -ex
-tags=${tags:-"centos6"}
 
 if [ -n "${DOCKER_REGISTRY_HOST}" ]; then
-  for t in ${tags}; do
-    docker tag italiangrid/storm-testsuite:${t} ${DOCKER_REGISTRY_HOST}/italiangrid/storm-testsuite:${t}
-    docker push ${DOCKER_REGISTRY_HOST}/italiangrid/storm-testsuite:${t}
-  done
+  docker push ${DOCKER_REGISTRY_HOST}/italiangrid/storm-testsuite
 fi
