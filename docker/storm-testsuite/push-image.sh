6bd0d1acb84e919407907bf7bd8a5f7f867ce7f8
enricovianello <maverick17586@gmail.com>
2018-06-01 17:35:47 +0200

    fixed push-image of storm-testsuite

--- docker/storm-testsuite/push-image.sh
+++ docker/storm-testsuite/push-image.sh
@@ -2,5 +2,6 @@
 set -ex
 
 if [ -n "${DOCKER_REGISTRY_HOST}" ]; then
+  docker tag italiangrid/storm-testsuite ${DOCKER_REGISTRY_HOST}/italiangrid/storm-testsuite
   docker push ${DOCKER_REGISTRY_HOST}/italiangrid/storm-testsuite
 fi
