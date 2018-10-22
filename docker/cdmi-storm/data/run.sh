843cd345722a0bff547dd440b3973ceaf9eafddc
enricovianello <maverick17586@gmail.com>
2018-10-22 16:13:28 +0200

    Fixed cdmi configuration files

--- docker/cdmi-storm/data/run.sh
+++ docker/cdmi-storm/data/run.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -x
 
-MODE="${MODE:-clean}"
-PLATFORM="${PLATFORM:-centos6}"
-REDIS_HOSTNAME="${REDIS_HOSTNAME:-localhost}"
+REDIS_HOSTNAME="${REDIS_HOSTNAME:-redis-server.example}"
 CDMI_CLIENT_ID="${CDMI_CLIENT_ID:-838129a5-84ca-4dc4-bfd8-421ee317aabd}"
 if [ -z ${CDMI_CLIENT_SECRET+x} ]; then echo "CDMI_CLIENT_SECRET is unset"; exit 1; fi
 
-DEPLOYMENT_SCRIPT="${MODE}-deployment_${PLATFORM}.sh"
-chmod +x ${DEPLOYMENT_SCRIPT}
+chmod +x deploy.sh
 
 export CDMI_CLIENT_ID
 export CDMI_CLIENT_SECRET
 
-./$DEPLOYMENT_SCRIPT
+./deploy.sh
