55dad9781ee4e87e9913b159e8fd200e50218a97
enricovianello <maverick17586@gmail.com>
2019-12-13 16:55:37 +0100

    upgrade from stable to beta test with puppet ok

--- docker/storm-deployment/assets/scripts/nightly/post-install.sh
+++ docker/storm-deployment/assets/scripts/nightly/post-install.sh
@@ -5,12 +5,3 @@ trap "exit 1" TERM
 # StoRM WebDAV needs to know what host has not to be considered as a 3rd-party copy
 echo ""  >> /etc/sysconfig/storm-webdav
 echo "STORM_WEBDAV_HOSTNAME_0=\"storm.example\"" >> /etc/sysconfig/storm-webdav
-
-# Explicit set conscrypt use to true
-echo ""  >> /etc/sysconfig/storm-webdav
-echo "STORM_WEBDAV_USE_CONSCRYPT=\"true\"" >> /etc/sysconfig/storm-webdav
-
-# Add TLS debug
-sed '/^STORM_WEBDAV_JVM_OPTS/ d'
-echo ""  >> /etc/sysconfig/storm-webdav
-echo "STORM_WEBDAV_JVM_OPTS=\"-Xms256m -Xmx512m -Djava.io.tmpdir=/var/lib/storm-webdav/work -Djavax.net.debug=all\"" >> /etc/sysconfig/storm-webdav
