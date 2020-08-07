bb95dd2b9d6de5960d44acc06b918a3e4532e818
enricovianello <maverick17586@gmail.com>
2020-08-07 16:40:07 +0200

    Added docker images build scripts

---
+++ docker/systemd-node/push-image.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -ex
+
+echo "Pushing italiangrid/systemd-node on dockerhub ..."
+docker push italiangrid/systemd-node
---
+++ docker/tf-node/push-image.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -ex
+
+echo "Pushing italiangrid/tf-node on dockerhub ..."
+docker push italiangrid/tf-node
