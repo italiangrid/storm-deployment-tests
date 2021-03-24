1d0ee9cc75e1214acd91202dbe366e00820d17e1
enricovianello <maverick17586@gmail.com>
2021-03-24 23:10:05 +0100

    Use centos7java11

--- all-in-one/centos7/assets/node/configure-node.sh
+++ all-in-one/centos7/assets/node/configure-node.sh
@@ -3,6 +3,7 @@
 STORM_TARGET_RELEASE=${STORM_TARGET_RELEASE:-"stable"}
 VOMS_TARGET_RELEASE=${VOMS_TARGET_RELEASE:-"stable"}
 PKG_STORM_BRANCH=${PKG_STORM_BRANCH:-"none"}
+PKG_STORM_PLATFORM=${PKG_STORM_PLATFORM:-"centos7java11"}
 PKG_VOMS_BRANCH=${PKG_VOMS_BRANCH:-"none"}
 
 echo "STORM_TARGET_RELEASE=${STORM_TARGET_RELEASE}"
@@ -66,14 +67,14 @@ if [ "$PKG_STORM_BRANCH" != "none" ]; then
 yumrepo { 'pkg-storm-BRANCH_NAME':
   ensure   => present,
   descr    => 'pkg-storm-BRANCH_NAME',
-  baseurl  => 'https://ci.cloud.cnaf.infn.it/job/pkg.storm/job/BRANCH_NAME/lastSuccessfulBuild/artifact/artifacts/stage-area/centos7/',
+  baseurl  => 'https://ci.cloud.cnaf.infn.it/job/pkg.storm/job/BRANCH_NAME/lastSuccessfulBuild/artifact/artifacts/stage-area/PKG_STORM_PLATFORM/',
   enabled  => 1,
   protect  => 1,
   priority => 1,
   gpgcheck => 0,
 }
 EOM
-    PKG_STORM_PP=`echo "${PKG_STORM_PP_TEMPLATE}" | sed -e "s/BRANCH_NAME/${PKG_STORM_BRANCH}/g"`
+    PKG_STORM_PP=`echo "${PKG_STORM_PP_TEMPLATE}" | sed -e "s/BRANCH_NAME/${PKG_STORM_BRANCH}/g" | sed -e "s/PKG_STORM_PLATFORM/${PKG_STORM_PLATFORM}/g"`
     echo "${PKG_STORM_PP}" > /assets/node/repos/pkg-storm.pp
     puppet apply /assets/node/repos/pkg-storm.pp
 fi
