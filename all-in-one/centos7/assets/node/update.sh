dd1ccb0042fb36f6c099ae2e218d18aa0d686d16
Enrico Vianello <maverick17586@gmail.com>
2021-10-25 00:04:59 +0200

    Add repo from PKG in the right place

--- all-in-one/centos7/assets/node/update.sh
+++ all-in-one/centos7/assets/node/update.sh
@@ -4,6 +4,40 @@
 puppet apply /assets/node/repos/storm/${STORM_TARGET_RELEASE}.pp
 puppet apply /assets/node/repos/voms/${VOMS_TARGET_RELEASE}.pp
 
+if [ "$PKG_STORM_BRANCH" != "none" ]; then
+    read -r -d '' PKG_STORM_PP_TEMPLATE << EOM
+yumrepo { 'pkg-storm-BRANCH_NAME':
+  ensure   => present,
+  descr    => 'pkg-storm-BRANCH_NAME',
+  baseurl  => 'https://ci.cloud.cnaf.infn.it/job/pkg.storm/job/BRANCH_NAME/lastSuccessfulBuild/artifact/artifacts/stage-area/PKG_STORM_PLATFORM/',
+  enabled  => 1,
+  protect  => 1,
+  priority => 1,
+  gpgcheck => 0,
+}
+EOM
+    PKG_STORM_PP=`echo "${PKG_STORM_PP_TEMPLATE}" | sed -e "s/BRANCH_NAME/${PKG_STORM_BRANCH}/g" | sed -e "s/PKG_STORM_PLATFORM/${PKG_STORM_PLATFORM}/g"`
+    echo "${PKG_STORM_PP}" > /assets/node/repos/pkg-storm.pp
+    puppet apply /assets/node/repos/pkg-storm.pp
+fi
+
+if [ "$PKG_VOMS_BRANCH" != "none" ]; then
+    read -r -d '' PKG_VOMS_PP_TEMPLATE << EOM
+yumrepo { 'pkg-voms-BRANCH_NAME':
+  ensure   => present,
+  descr    => 'pkg-voms-BRANCH_NAME',
+  baseurl  => 'https://ci.cloud.cnaf.infn.it/job/pkg.voms/job/BRANCH_NAME/lastSuccessfulBuild/artifact/artifacts/stage-area/centos7/',
+  enabled  => 1,
+  protect  => 1,
+  priority => 1,
+  gpgcheck => 0,
+}
+EOM
+    PKG_VOMS_PP=`echo "${PKG_VOMS_PP_TEMPLATE}" | sed -e "s/BRANCH_NAME/${PKG_VOMS_BRANCH}/g"`
+    echo "${PKG_VOMS_PP}" > /assets/node/repos/pkg-voms.pp
+    puppet apply /assets/node/repos/pkg-voms.pp
+fi
+
 puppet module uninstall cnafsd-storm
 # Install storm puppet module
 if [ -d "/storm-puppet-module" ] 
